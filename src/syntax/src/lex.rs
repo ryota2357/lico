6be670152224b9ae96aa716fb829a3d3546598c3
2357 <61523777+ryota2357@users.noreply.github.com>
2023-08-30 23:00:19 +0900

    feat: impl Copy and Display trait to Token

--- src/syntax/src/lex.rs
+++ src/syntax/src/lex.rs
@@ -3,7 +3,7 @@ use chumsky::prelude::*;
 type Span = SimpleSpan<usize>;
 type Error<'a> = Rich<'a, char, Span>;
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Token<'src> {
     // literals
     Int(i64),
@@ -61,6 +61,58 @@ pub enum Token<'src> {
 
     // other
     Identifier(&'src str),
+}
+
+impl std::fmt::Display for Token<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Token::Int(x) => write!(f, "{}", x),
+            Token::Float(x) => write!(f, "{}", x),
+            Token::String(x) => write!(f, "\"{}\"", x),
+            Token::Bool(x) => write!(f, "{}", if *x { "true" } else { "false" }),
+            Token::Nil => write!(f, "nil"),
+            Token::Var => write!(f, "var"),
+            Token::Let => write!(f, "let"),
+            Token::Func => write!(f, "func"),
+            Token::If => write!(f, "if"),
+            Token::Then => write!(f, "then"),
+            Token::Elif => write!(f, "elif"),
+            Token::Else => write!(f, "else"),
+            Token::For => write!(f, "for"),
+            Token::While => write!(f, "while"),
+            Token::In => write!(f, "in"),
+            Token::Do => write!(f, "do"),
+            Token::End => write!(f, "end"),
+            Token::Return => write!(f, "return"),
+            Token::Break => write!(f, "break"),
+            Token::Continue => write!(f, "continue"),
+            Token::Add => write!(f, "+"),
+            Token::Sub => write!(f, "-"),
+            Token::Mul => write!(f, "*"),
+            Token::Div => write!(f, "/"),
+            Token::Mod => write!(f, "%"),
+            Token::Pow => write!(f, "^"),
+            Token::Eq => write!(f, "=="),
+            Token::NotEq => write!(f, "!="),
+            Token::Less => write!(f, "<"),
+            Token::LessEq => write!(f, "<="),
+            Token::Greater => write!(f, ">"),
+            Token::GreaterEq => write!(f, ">="),
+            Token::Dot => write!(f, "."),
+            Token::Assign => write!(f, "="),
+            Token::And => write!(f, "and"),
+            Token::Or => write!(f, "or"),
+            Token::Not => write!(f, "not"),
+            Token::Comma => write!(f, ","),
+            Token::OpenParen => write!(f, "("),
+            Token::CloseParen => write!(f, ")"),
+            Token::OpenBrace => write!(f, "{{"),
+            Token::CloseBrace => write!(f, "}}"),
+            Token::OpenBracket => write!(f, "["),
+            Token::CloseBracket => write!(f, "]"),
+            Token::Identifier(x) => write!(f, "{}", x),
+        }
+    }
 }
 
 pub fn lexer<'src>(
