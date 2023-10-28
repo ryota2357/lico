6a99363a5265f852ac4b8edb2d94868b0dbd1581
2357 <61523777+ryota2357@users.noreply.github.com>
2023-10-28 18:09:51 +0900

    test(parser): fix due to lexer update

--- src/parser/tests/common/mod.rs
+++ src/parser/tests/common/mod.rs
@@ -1,6 +1,6 @@
 use parser::tree::Program;
 
 pub fn parse_program(src: &str) -> Program<'_> {
-    let tokens = lexer::parse(src);
+    let (tokens, _) = lexer::parse(src);
     return parser::parse(&tokens, src.len()..src.len());
 }
