484d8776eb6df3acf7d9b98349ca0a8667249171
2357 <61523777+ryota2357@users.noreply.github.com>
2023-12-06 22:49:42 +0900

    feat(cli): display compile error pos

--- cli/run/src/lib.rs
+++ cli/run/src/lib.rs
@@ -24,8 +24,57 @@ pub fn start(file: &PathBuf) {
         return;
     };
 
-    let code = compiler::compile(&tree).unwrap();
+    let code = match compiler::compile(&tree) {
+        Ok(x) => x,
+        Err(e) => {
+            println!("Compilation error: {:?}", e);
+            let (start, end) = match get_line_column_range(buf_str, e.span) {
+                Some(x) => x,
+                None => {
+                    println!("Invalid span");
+                    return;
+                }
+            };
+            println!("Positon: {}:{} ~ {}:{}", start.0, start.1, end.0, end.1);
+            return;
+        }
+    };
 
     let mut runtime = vm::runtime::Runtime::new(std::io::stdout());
     vm::execute(&code, &mut runtime).unwrap();
+}
+
+fn get_line_column_range(
+    source: &str,
+    span: std::ops::Range<usize>,
+) -> Option<((usize, usize), (usize, usize))> {
+    let source_len = source.chars().count();
+
+    // Return None if the start and end positions of the range are outside the document range
+    if span.start >= source_len || span.end > source_len || span.start > span.end {
+        return None;
+    }
+
+    let line_start_offsets = {
+        let mut offsets = vec![0];
+        for (i, c) in source.char_indices() {
+            if c == '\n' {
+                offsets.push(i + 1);
+            }
+        }
+        offsets
+    };
+
+    let calc_line_column = |offset: usize| {
+        let line = line_start_offsets
+            .iter()
+            .position(|&line_start_offset| line_start_offset > offset)
+            .unwrap_or(line_start_offsets.len() - 1);
+        let column = offset - line_start_offsets[line - 1];
+        (line, column)
+    };
+
+    let start = calc_line_column(span.start);
+    let end = calc_line_column(span.end);
+    Some((start, end))
 }
