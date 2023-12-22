dd219dfcb54896a6e9422fc0db5c254cda148abc
2357 <61523777+ryota2357@users.noreply.github.com>
2023-12-23 01:02:53 +0900

    remove parser

--- src/parser/tests/common/mod.rs
+++
@@ -1,7 +0,0 @@
-use parser::Program;
-
-pub fn parse_program(src: &str) -> Program<'_> {
-    let (tokens, _) = lexer::parse(src);
-    let (program, _) = parser::parse(&tokens, src.len()..src.len());
-    program.unwrap()
-}
