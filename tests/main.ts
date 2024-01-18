1b88dd918ddc5595550523df757082ae6475f56d
2357 <61523777+ryota2357@users.noreply.github.com>
2024-01-18 14:59:13 +0900

    test: upgrade test scripts

--- tests/main.ts
+++ tests/main.ts
@@ -1,7 +1,7 @@
-import * as path from "https://deno.land/std@0.208.0/path/mod.ts";
-import * as fs from "https://deno.land/std@0.208.0/fs/mod.ts";
-import * as colors from "https://deno.land/std@0.208.0/fmt/colors.ts";
-import { printf } from "https://deno.land/std@0.208.0/fmt/printf.ts";
+import * as path from "https://deno.land/std@0.212.0/path/mod.ts";
+import * as fs from "https://deno.land/std@0.212.0/fs/mod.ts";
+import * as colors from "https://deno.land/std@0.212.0/fmt/colors.ts";
+import { printf } from "https://deno.land/std@0.212.0/fmt/printf.ts";
 
 const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
 
@@ -11,145 +11,213 @@ async function main() {
     console.error(colors.red("Mode must be 'release' or 'debug'"));
     Deno.exit(1);
   }
+  const checker = new Checker(mode);
 
-  const run = await build_run_command(mode);
-
-  // Run a specific test case
+  const all_test_groups = [];
   if (Deno.args[1] !== undefined) {
-    const [group, name] = Deno.args[1].split("/");
-    const test_case = (await get_test_cases(group)).find((x) => x.name == name);
-    if (test_case === undefined) {
-      printf(
-        colors.red("Test case %s in %s not found.\n"),
-        colors.bold(name),
-        colors.bold(group),
-      );
-      Deno.exit(1);
-    }
-    const output = await run(test_case.path, test_case.input);
-    if (output != test_case.output ?? "") {
-      console.error(colors.red("Failed"));
-      console.error(colors.gray(colors.italic("Output:")));
-      console.error(output);
-      Deno.exit(1);
+    // When a test case is specified.
+    const [group, name] = Deno.args[1].split("/") as [string, string?];
+    if (name !== undefined) {
+      const test_case = await get_test_case(group, name);
+      all_test_groups.push({
+        name: group,
+        path: path.join(__dirname, "cases", group),
+        cases: [test_case],
+      });
     } else {
-      console.log(colors.green("Passed"));
+      const test_group = await get_test_group(group);
+      all_test_groups.push(test_group);
     }
-    return;
+  } else {
+    for await (
+      const entry of fs.expandGlob(path.join(__dirname, "cases", "*"))
+    ) {
+      if (entry.isDirectory) {
+        const test_group = await get_test_group(entry.name);
+        all_test_groups.push(test_group);
+      }
+    }
+  }
+  all_test_groups.sort(() => 0.5 - Math.random()); // shuffle
+
+  const count = { ok: 0, ng: 0 };
+  const ng_results: Extract<TestResult, { status: "ng" }>[] = [];
+  console.log(`\nRunning tests in ${colors.cyan(mode)} mode...\n`);
+  for (const test_group of all_test_groups) {
+    for await (const result of checker.test(test_group)) {
+      if (result.status === "ok") {
+        printf("[%s] %s\n", colors.green("ok"), result.name);
+        count.ok += 1;
+      } else {
+        printf("[%s] %s\n", colors.red("ng"), result.name);
+        count.ng += 1;
+        ng_results.push(result);
+      }
+    }
   }
 
-  console.log(`\nRunning tests in ${colors.cyan(mode)} mode...\n`);
-
-  const test_all = async function (group: string) {
-    const ngs: { name: string; actual: string; expected: string }[] = [];
-
-    printf("%s\n", colors.gray(`Running test cases in '${group}'`));
-    for (const test_case of await get_test_cases(group)) {
-      printf("%s ...", test_case.name);
-      const output = await run(test_case.path, test_case.input);
-      if (output !== (test_case.output ?? "")) {
-        printf(colors.red(" ng\n"));
-        ngs.push({
-          name: test_case.name,
-          actual: output,
-          expected: test_case.output ?? "",
-        });
+  printf("\n%s\n", colors.gray("Test result:"));
+  printf("  %s: %d\n", colors.green("ok"), count.ok);
+  printf("  %s: %d\n", colors.red("ng"), count.ng);
+  if (count.ng == 0) {
+    printf("\n%s\n", colors.green("All tests passed!"));
+  } else {
+    printf("\n%s\n", colors.red("Some tests failed."));
+    const snip = function (s: string) {
+      if (s.length > 100) {
+        const rest = s.length - 100;
+        return s.substring(0, 100) + colors.gray(` ... and ${rest} more`);
       } else {
-        printf(colors.green(" ok\n"));
+        return s;
       }
+    };
+    for (const result of ng_results) {
+      printf("\n%s\n", colors.inverse(result.name));
+      printf(colors.gray(colors.italic("Expected:")));
+      printf("\n%s\n", snip(result.expected));
+      printf(colors.gray(colors.italic("Got:")));
+      printf("\n%s\n", snip(result.actual));
+      console.error(
+        colors.gray("For more detail: ") +
+          colors.bold(`deno task test ${mode} ${result.name}`),
+      );
     }
-
-    if (ngs.length > 0) {
-      console.error(colors.red("\nFailed test cases:"));
-      const snip = function (s: string) {
-        if (s.length > 100) {
-          const rest = s.length - 100;
-          return s.substring(0, 100) + colors.gray(` ... and ${rest} more`);
-        } else {
-          return s;
-        }
-      };
-      for (const ng of ngs) {
-        console.error();
-        console.error(colors.inverse(ng.name));
-        console.error(colors.gray(colors.italic("Expected:")));
-        console.error(snip(ng.expected));
-        console.error(colors.gray(colors.italic("Got:")));
-        console.error(snip(ng.actual));
-        console.error(
-          colors.gray("For more detail: ") +
-            colors.bold(`deno task test ${mode} ${group}/${ng.name}`),
-        );
-      }
-      Deno.exit(1);
-    }
-  };
-
-  await test_all("random");
+    Deno.exit(1);
+  }
 }
 
-async function get_test_cases(group: string) {
-  const test_cases: {
-    name: string;
-    path: string;
-    input?: string;
-    output?: string;
-  }[] = [];
+interface TestCase {
+  readonly name?: string;
+  readonly input?: string;
+  readonly output?: string;
+}
 
-  for await (
-    const file of fs.expandGlob(path.join(__dirname, group, "*.lico"))
-  ) {
-    const get = function (path: string) {
-      if (fs.existsSync(path)) {
-        return Deno.readTextFileSync(path);
+type BuildMode = "release" | "debug";
+
+interface TestGroup {
+  readonly name: string;
+  readonly path: string;
+  readonly cases: TestCase[];
+}
+
+async function read_file_safe(path: string): Promise<string | undefined> {
+  if (fs.existsSync(path)) {
+    return await Deno.readTextFile(path);
+  }
+  return undefined;
+}
+
+async function get_test_case(group: string, name: string): Promise<TestCase> {
+  const group_path = path.join(__dirname, "cases", group);
+  const input_path = path.join(group_path, "input", `${name}.txt`);
+  const output_path = path.join(group_path, "output", `${name}.txt`);
+  return {
+    name,
+    input: await read_file_safe(input_path),
+    output: await read_file_safe(output_path),
+  };
+}
+
+async function get_test_group(group_name: string): Promise<TestGroup> {
+  const group_path = path.join(__dirname, "cases", group_name);
+
+  const input_txt = await read_file_safe(path.join(group_path, "input.txt"));
+  const output_txt = await read_file_safe(path.join(group_path, "output.txt"));
+  if (input_txt !== undefined || output_txt !== undefined) {
+    return {
+      name: group_name,
+      path: group_path,
+      cases: [{
+        name: undefined,
+        input: input_txt,
+        output: output_txt,
+      }],
+    };
+  }
+
+  const all_test_names = await (async function () {
+    const collect_test_names = async function (type: "input" | "output") {
+      const iter = fs.expandGlob(path.join(group_path, type, "*.txt"));
+      const entries = await Array.fromAsync(iter);
+      return entries.map((x) => x.name.replace(/\.txt$/, ""));
+    };
+    const input_names = await collect_test_names("input");
+    const output_names = await collect_test_names("output");
+    const names = new Set([...input_names, ...output_names]);
+    return Array.from(names);
+  })();
+
+  const test_cases: TestCase[] = [];
+  for (const name of all_test_names) {
+    const test_case = await get_test_case(group_name, name);
+    test_cases.push(test_case);
+  }
+  test_cases.sort(() => 0.5 - Math.random()); // shuffle
+
+  return {
+    name: group_name,
+    path: group_path,
+    cases: test_cases,
+  };
+}
+
+type TestResult = {
+  readonly name: string;
+  readonly status: "ok";
+} | {
+  readonly name: string;
+  readonly status: "ng";
+  readonly actual: string;
+  readonly expected: string;
+};
+
+class Checker {
+  readonly mode: BuildMode;
+  readonly #lico_path: string;
+  readonly #textDecoder = new TextDecoder();
+  readonly #textEncoder = new TextEncoder();
+
+  constructor(mode: BuildMode) {
+    this.mode = mode;
+    this.#lico_path = cargo_build(mode);
+  }
+
+  async *test(group: TestGroup): AsyncGenerator<TestResult> {
+    const target = path.join(group.path, "main.lico");
+
+    const make_name = function (name?: string) {
+      if (name === undefined) {
+        return group.name;
+      } else {
+        return `${group.name}/${name}`;
       }
-      return undefined;
     };
 
-    const name = file.name.replace(/\.lico$/, "");
-    test_cases.push({
-      name,
-      path: file.path,
-      input: get(path.join(__dirname, group, "in", name + ".txt")),
-      output: get(path.join(__dirname, group, "out", name + ".txt")),
-    });
-  }
-  test_cases.sort((_, __) => 0.5 - Math.random()); // shuffle
-  return test_cases;
-}
-
-async function build_run_command(mode: "release" | "debug") {
-  const cli_path = path.join(__dirname, "..", "cli");
-
-  const build_command = new Deno.Command("cargo", {
-    args: [
-      "build",
-      mode == "release" ? "--release" : undefined,
-      "--manifest-path",
-      path.join(cli_path, "Cargo.toml"),
-    ].filter((x) => x !== undefined) as string[],
-    stdout: "inherit",
-    stderr: "inherit",
-  });
-  const { code } = await build_command.output();
-  if (code != 0) {
-    console.error(`Exit code of \`cargo build\` is expected 0 but got ${code}`);
+    for (const test_case of group.cases) {
+      const output = await this.#run(target, test_case.input);
+      if (output === (test_case.output ?? "")) {
+        yield {
+          name: make_name(test_case.name),
+          status: "ok",
+        };
+      } else {
+        yield {
+          name: make_name(test_case.name),
+          status: "ng",
+          actual: output,
+          expected: test_case.output ?? "",
+        };
+      }
+    }
   }
 
-  const textDecoder = new TextDecoder();
-  const textEncoder = new TextEncoder();
-  return async function (src: string, input?: string): Promise<string> {
+  async #run(source_code: string, input?: string): Promise<string> {
     const prosess = new Deno.Command(
-      path.join(
-        cli_path,
-        "target",
-        mode == "release" ? "release" : "debug",
-        "lico",
-      ),
+      this.#lico_path,
       {
         args: [
           "run",
-          src,
+          source_code,
         ],
         stdin: "piped",
         stdout: "piped",
@@ -159,7 +227,7 @@ async function build_run_command(mode: "release" | "debug") {
 
     if (input) {
       const writer = prosess.stdin.getWriter();
-      await writer.write(textEncoder.encode(input));
+      await writer.write(this.#textEncoder.encode(input));
       await writer.close();
     }
 
@@ -167,17 +235,47 @@ async function build_run_command(mode: "release" | "debug") {
     if (code != 0) {
       console.error(
         colors.red(
-          `\nWhile running ${src}, exit code is expected 0 but got ${code}`,
+          `\nWhile running ${source_code}, exit code is expected 0 but got ${code}`,
         ),
       );
     }
     if (stderr.length != 0) {
-      const err = textDecoder.decode(stderr);
+      const err = this.#textDecoder.decode(stderr);
       console.error(colors.italic(colors.gray("stderr:")));
       console.error(err);
     }
-    return textDecoder.decode(stdout);
-  };
+    return this.#textDecoder.decode(stdout);
+  }
+}
+
+/**
+ * Build `lico` and return the path to the executable.
+ */
+function cargo_build(mode: BuildMode): string {
+  const cli_path = path.join(__dirname, "..", "cli");
+  const cargo_build = new Deno.Command("cargo", {
+    args: [
+      "build",
+      mode == "release" ? "--release" : undefined,
+      "--manifest-path",
+      path.join(cli_path, "Cargo.toml"),
+    ].filter((x) => x !== undefined) as string[],
+    stdout: "inherit",
+    stderr: "inherit",
+  });
+  const { code } = cargo_build.outputSync();
+  if (code != 0) {
+    console.error(
+      `Exit code of \`cargo build\` is expected 0 but got ${code}`,
+    );
+    Deno.exit(1);
+  }
+  return path.join(
+    cli_path,
+    "target",
+    mode == "release" ? "release" : "debug",
+    "lico",
+  );
 }
 
 await main();
