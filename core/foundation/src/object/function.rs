33073fd5ddd51690f4d8572194d8ced89e41ed9e
ryota2357 <61523777+ryota2357@users.noreply.github.com>
2024-08-15 20:30:54 +0900

    fix(foundation): fix object::Function implemention

--- core/foundation/src/object/function.rs
+++ core/foundation/src/object/function.rs
@@ -1,34 +1,32 @@
-use super::Object;
+use super::*;
 use crate::il::Executable;
-use core::ptr::addr_eq;
-use std::{cell::RefCell, rc::Rc};
+use core::cell::{Cell, RefCell};
+use std::rc::Rc;
 
 #[derive(Clone, Debug)]
-pub struct Function(Box<Inner>);
+pub struct Function(Rc<Inner>);
 
 #[derive(Clone, Debug)]
 struct Inner {
     exe: Executable,
-    start_index: usize,
     env: Box<[Rc<RefCell<Object>>]>,
-    call_count: u32,
+    param_len: u8,
+    start_index: usize,
+    call_count: Cell<u32>,
 }
 
 impl Function {
-    pub fn new<I>(exe: Executable, start: usize, env: I) -> Self
+    pub fn new<I>(exe: Executable, param_len: u8, start_index: usize, env: I) -> Self
     where
         I: IntoIterator<Item = Rc<RefCell<Object>>>,
     {
-        Function(Box::new(Inner {
+        Function(Rc::new(Inner {
             exe,
             env: env.into_iter().collect(),
-            start_index: start,
-            call_count: 0,
+            param_len,
+            start_index,
+            call_count: Cell::new(0),
         }))
-    }
-
-    pub fn start_index(&self) -> usize {
-        self.0.start_index
     }
 
     pub fn executable(&self) -> &Executable {
@@ -39,20 +37,28 @@ impl Function {
         &self.0.env
     }
 
+    pub fn param_len(&self) -> u8 {
+        self.0.param_len
+    }
+
+    pub fn start_index(&self) -> usize {
+        self.0.start_index
+    }
+
     pub fn call_count(&self) -> u32 {
-        self.0.call_count
+        self.0.call_count.get()
     }
 
     pub fn inc_call_count(&mut self) {
-        self.0.call_count += 1;
+        let call_count = self.0.call_count.get().saturating_add(1);
+        self.0.call_count.set(call_count);
     }
 }
 
-// TODO: write the reason why we only compare the pointer instead of the content (Inner)
 impl PartialEq for Function {
     fn eq(&self, other: &Self) -> bool {
-        let self_ptr = &*self.0 as *const Inner;
-        let other_ptr = &*other.0 as *const Inner;
-        addr_eq(self_ptr, other_ptr)
+        Rc::ptr_eq(&self.0, &other.0)
     }
 }
+
+impl Eq for Function {}
